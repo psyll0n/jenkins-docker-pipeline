78471f2fd9b3b81961d22a1db3ef9e70fea239e5
psyll0n <45129366+psyll0n@users.noreply.github.com>
2021-06-03 19:24:00 +0300

    Update hello_there.py

--- hello_there.py
+++ hello_there.py
@@ -2,5 +2,5 @@
 #!python3
 # hello_there.py
 
-print("Hello there from Python, Docker and Jenkins!")
+print("Hello there from Python, Docker and Jenkins!!!")
 print("This is a simple Python app created with a CI/CD pipeline.")
